f4a8833777d26d3350de1e43673fa17ccfc05d2b
Daniel Schlegel <firmasaga@gmail.com>
2011-03-07 10:11:47 +0100

    bevore adding rails generators

--- lib/base_writer.rb
+++ lib/base_writer.rb
@@ -22,43 +22,35 @@ class BaseWriter
 
   def node_operations(node)
     str = process_nodes(node)
-    str[0..str.size-2]
+    str[1..str.size-2]
   end
 
   def process_nodes(node, out = "")
     sub_node = node.xpath("sv:node")
     temp_out = ""
 
+
+    node.xpath("sv:property").each do |p|
+      name = p.attr("name").to_s.gsub("stk", @project_prefix)
+      value = p.xpath("sv:value").text.to_s.gsub("stk", @project_prefix)
+      out += ",\naddProperty(\"#{name}\", \"#{value}\")"
+    end
+    out = @level > 0 ? out[1..out.size] : out
+    @level -= 1 if @level > 0
+
     if sub_node.size > 0
 
+      @level += 1
       sub_node.each do |sub_sub_node|
-        @level += 1
         name = sub_sub_node.attr("name").to_s.gsub("stk", @project_prefix)
         #TODO: make dynamic type
         value = "ItemType.CONTENTNODE" #sub_sub_node.xpath("sv:value").text.to_s.gsub("stk", @project_prefix)
-        temp_out += "addNode(\"#{name}\", #{value}).then(
+        temp_out += ",\naddNode(\"#{name}\", #{value}).then(
                             #{process_nodes(sub_sub_node, out)}
-                    ),\n"
+                    )"
       end
-      temp_out = @level > 0 ? temp_out[0..temp_out.size-3] : temp_out[0..temp_out.size-2]
-      @level -= 1 if @level > 0
-      node.xpath("sv:property").each do |p|
-        name = p.attr("name").to_s.gsub("stk", @project_prefix)
-        value = p.xpath("sv:value").text.to_s.gsub("stk", @project_prefix)
-        out += "addProperty(\"#{name}\", \"#{value}\"),\n"
-      end
-      out = out[0..out.size-3]
-      out += temp_out
-    else
-      node.xpath("sv:property").each do |p|
-        name = p.attr("name").to_s.gsub("stk", @project_prefix)
-        value = p.xpath("sv:value").text.to_s.gsub("stk", @project_prefix)
-        out += "addProperty(\"#{name}\", \"#{value}\"),\n"
-      end
-
-      out =  out[0..out.size-3]
-      @level -= 1 if @level > 0
-      out += temp_out 
+      temp_out = @level > 0 ? temp_out[0..temp_out.size-3] : temp_out
     end
+    out += temp_out
   end
 end
